5e3dac6ba0124fef46852b82fc670366b5bb616e
mccalluc <mccalluc@users.noreply.github.com>
2022-06-03 22:46:01 -0400

    Use cached CDN URLs

--- docs/assets/js/upstream.js
+++ docs/assets/js/upstream.js
@@ -6,8 +6,9 @@
 // and I'm not sure how to get that conditionality in here without breaking mocha,
 // so it's still pulled in as global in the HTML.
 
-export {default as md5} from 'https://cdn.skypack.dev/js-md5@0.7.3';
-export {getDistance, getCompassDirection} from 'https://cdn.skypack.dev/geolib@3.3.3';
+export {default as md5} from 'https://cdn.skypack.dev/pin/js-md5@v0.7.3-ZZMBYZMNbuXkM7xKSyTL/mode=imports,min/optimized/js-md5.js';
+// 'https://cdn.skypack.dev/js-md5@0.7.3';
+export {getDistance, getCompassDirection} from 'https://cdn.skypack.dev/pin/geolib@v3.3.3-mscC4dWBwso83R4wkEjM/mode=imports,min/optimized/geolib.js';
+// 'https://cdn.skypack.dev/geolib@3.3.3';
 
-// TODO: Use optimized URLs
-// TODO: Just download part of geolib.
+// TODO: Just download part of geolib?
