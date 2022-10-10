c9caf8ba715fc4bc96e5f7b4fb0631f21ab9dac9
mccalluc <mccalluc@users.noreply.github.com>
2022-10-10 17:49:01 -0400

    cleanup copy+paste

--- docs/assets/js/components/GetConsent.js
+++ docs/assets/js/components/GetConsent.js
@@ -7,70 +7,7 @@ export default {
       hasConsented: localStorage.hasConsented,
     }
   },
-  computed: {
-    conversionFactor() {
-      return {
-        [MILE]: 1/1609,
-        [KM]: 1/1000
-      }[this.unit]
-    },
-    distanceInMeters() {
-      try {
-        return geolib.getDistance(ll(this.hereLatLong), ll(this.goalLatLong)) - this.radiusInMeters;
-      } catch {
-        return undefined;
-      }
-    },
-    radiusInMeters() {
-      return this.radius / this.conversionFactor;
-    },
-    compassDirection() {
-      try {
-        return geolib.getCompassDirection(ll(this.hereLatLong), ll(this.goalLatLong));
-      } catch {
-        return undefined;
-      }
-    },
-  },
   methods: {
-    async updateHere(addToAttempts = true) {
-      this.hereLatLong = await getLatLong();
-      const attemptsCount = this.attempts.length;
-      const distanceDirection = `${
-        (this.distanceInMeters * this.conversionFactor).toPrecision(2)
-      } ${this.unit} ${this.compassDirection}`;
-
-      if (!addToAttempts) {
-        this.attempts[0] = {
-          message: `Walk ${distanceDirection}`,
-          distanceInMeters: this.distanceInMeters,
-        };
-      } else {
-        if (this.radiusInMeters > this.distanceInMeters) {
-          this.isGoalReached = true;
-          this.attempts.unshift({
-            isGoalReached: true,
-            count: attemptsCount,
-          });
-        } else {
-          let temperature = '';
-          const hasMoved = !this.attempts[0].message.includes(distanceDirection);
-          if (hasMoved) {
-            temperature = this.distanceInMeters < this.attempts[0].distanceInMeters
-              ? '🔥 warmer'
-              : '🧊 cooler';
-          }
-          this.attempts.unshift({
-            message: `${hasMoved ? 'Now go' : 'Still'} ${distanceDirection}`,
-            count: attemptsCount,
-            distanceInMeters: this.distanceInMeters,
-            temperature
-          });
-        }
-      }
-
-      localStorage.setItem('attempts', JSON.stringify(this.attempts))
-    },
     setHasConsented() {
       localStorage.setItem('hasConsented', true);
       this.hasConsented = true;
@@ -90,7 +27,7 @@ export default {
         If you click "OK", your browser will prompt you to confirm.
         <button 
           @click="setHasConsented()"
-          class="btn btn-sm btn-outline-dark">
+          class="btn btn-sm btn-outline-dark px-1 py-0">
           OK
         </button>
       </div>
